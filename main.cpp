9e2c0fdac2c3762173f2cbabe040551f828ba4e0
damazicando <93984039@qq.com>
2019-02-14 15:12:25 +0800

    版本1
    第一次提交

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "multithread_frame.h"
+
+using namespace std;
+
+class SampleData
+{
+public:
+    int seq;
+    int ms;
+};
+
+int main()
+{
+    MultiThreadFrame<SampleData> mtf;
+    std::mutex mtx;
+
+    auto pfunc = [&mtx](const SampleData &d){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        std::unique_lock<std::mutex> lock_(mtx);
+        std::cout << "thread " << std::this_thread::get_id() << " do work " << d.seq << " " <<
+                     std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count() << std::endl;
+    };
+    mtf.Start(3, pfunc);
+
+    for(int i=0; i<100; i++)
+    {
+        SampleData d;
+        d.seq = i;
+        mtf.Push(d);
+    }
+
+    int x;
+    std::cin >> x;
+
+    return 0;
+}
---
+++ multithread_frame.h
@@ -0,0 +1,69 @@
+﻿#ifndef _MULTI_THREAD_FRAME_H_
+#define _MULTI_THREAD_FRAME_H_
+
+#include <thread>
+#include <future>
+#include <vector>
+#include <iostream>
+#include "thread_queue.h"
+#include <algorithm>
+
+template <typename T>
+class MultiThreadFrame
+{
+public:
+    void Start(int thread_num, std::function<void(const T &t)> func)
+    {
+        func_ = func;
+        terminate_ = false;
+        for(int i=0; i<thread_num; i++)
+        {
+            std::future<void> ftr = std::async(std::launch::async, &MultiThreadFrame::run, this);
+            vftr_.push_back(std::move(ftr));
+        }
+    }
+
+    void Stop()
+    {
+        terminate_ = true;
+        for (auto &ftr : vftr_)
+        {
+            ftr.wait();
+        }
+    }
+
+    void Push(const T &t)
+    {
+        mq_.push_back(t);
+    }
+private:
+    void run()
+    {
+        while(!terminate_)
+        {
+            try
+            {
+                T t;
+                if (mq_.pop_front(t, 500))
+                {
+                    func_(t);
+                }
+            }
+            catch (std::exception &ex)
+            {
+                std::cout << ex.what() << std::endl;
+            }
+            catch (...)
+            {
+
+            }
+        }
+    }
+private:
+    std::atomic_bool terminate_;
+    ThreadQueue<T> mq_;
+    std::function<void(const T &t)> func_;
+    std::vector<std::future<void>> vftr_;
+};
+
+#endif // MULTI_THREAD_FRAME_H
